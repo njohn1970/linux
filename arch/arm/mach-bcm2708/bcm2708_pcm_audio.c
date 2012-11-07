32b1931d6cb97ee7323e4d94fa50003c4ca0ba35
Neil Johnson <neil@emailus.me.uk>
2012-11-07 20:19:22 +0000

    changes

--- arch/arm/mach-bcm2708/bcm2708_pcm_audio.c
+++ arch/arm/mach-bcm2708/bcm2708_pcm_audio.c
@@ -11,17 +11,63 @@
 #include <linux/device.h>
 #include <linux/platform_device.h>
 #include <linux/module.h>
+#include <linux/err.h>
+#include <linux/slab.h>
 
 #include <mach/pcm_audio.h>
 
 #define DRIVER_NAME BCM_PCM_DRIVER_NAME
 
+struct bcm_pcm_drv_context
+{
+    void __iomem *reg_base;
+};
+
+struct bcm_pcm_drv_context* bcm_pcm_alloc_drv_context(void)
+{
+    return NULL;
+}
 
 static int bcm_pcm_probe(struct platform_device *pdev)
 {
+    int ret = 0;
+    struct bcm_pcm_drv_context* drv_context;
+	struct resource* pcm_res = NULL;
 
+    drv_context = bcm_pcm_alloc_drv_context();
+    if( IS_ERR(drv_context) )
+    {
+        ret = PTR_ERR(drv_context);
+        drv_context = NULL;
+        goto out;
+    }
+    kfree( drv_context );
+    drv_context = NULL;
 
-    return -1;
+    pcm_res = platform_get_resource( pdev, IORESOURCE_MEM, 0 );
+    if(!pcm_res)
+    {
+        printk(KERN_ERR DRIVER_NAME": failed to get IO_MEM resource\n");
+        ret = -ENODEV;
+        goto out;
+    }
+
+    printk(KERN_INFO DRIVER_NAME": IO_MEM Start:0x%08X Size:0x%08X\n", pcm_res->start,
+            resource_size(pcm_res));
+
+out:
+
+    if( !ret )
+    {
+        /*Clean up*/
+
+        if( drv_context )
+        {
+            kfree( drv_context );
+        }
+    }
+
+    return ret;
 }
 
 static int bcm_pcm_remove(struct platform_device *pdev)
